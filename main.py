2b73325a016d66abe441fd2e75d53cac1afda774
nsurratt <nicholassurratt@gmail.com>
2024-09-25 16:23:26 -0400

    EOD Push - 9/25/2024

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ def main():
     print(f"{num_words} words found in the document")
     print(f"{char_counts}")
 
+# Function to sort the char_count dictionary.
+def sort_on(dict):
+    return dict["num"]
+
+# Function to count the number of words within the book / text
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+# Function to count the occurrence of each letter within the book / text
 def get_character_count(text):
     text = text.lower()
     letters = {}
@@ -17,6 +23,7 @@ def get_character_count(text):
         letters[i] = letters.get(i, 0) + 1
     return letters
 
+# Function to pull the book / text into the program
 def get_book_text(path):
     with open(path) as f:
         return f.read()
