d6930a2acece889ad0b53cffb405de5525bf1f70
nsurratt <nicholassurratt@gmail.com>
2024-09-26 10:53:32 -0400

    Character Report

--- main.py
+++ main.py
@@ -2,28 +2,49 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_counts = get_character_count(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(f"{char_counts}")
+    print()
 
-# Function to sort the char_count dictionary.
-def sort_on(dict):
-    return dict["num"]
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-# Function to count the number of words within the book / text
+    print("--- End report ---")
+
+
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-# Function to count the occurrence of each letter within the book / text
-def get_character_count(text):
-    text = text.lower()
-    letters = {}
-    for i in text:
-        letters[i] = letters.get(i, 0) + 1
-    return letters
 
-# Function to pull the book / text into the program
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
